2068ba36c47456728d653860cbb30b708324f49f
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-16 15:33:51 -0300

    feat: dwarf entity

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  private _maxLifePoints = 80;
+  private static _instances = 0;
+
+  constructor(
+    name:string,
+    dexterity: number,
+  ) {
+    super(name, dexterity);
+    Dwarf._instances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances():number {
+    return Dwarf._instances;
+  }
+}
