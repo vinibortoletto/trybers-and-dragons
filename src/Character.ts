bb94d889ba20ada838ba37b03f5c3eeb6fad6408
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-17 14:35:22 -0300

    fix: character name

--- src/Character.ts
+++ src/Character.ts
@@ -18,8 +18,8 @@ export default class Character implements Fighter {
   constructor(name: string) {
     this._name = name;
     this._dexterity = getRandomInt(1, 10);
-    this._race = new Elf('Legolas', this._dexterity);
-    this._archetype = new Mage('Gandalf');
+    this._race = new Elf(this._name, this._dexterity);
+    this._archetype = new Mage(this._name);
     this._maxLifePoints = this._race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
     this._strength = getRandomInt(1, 10);
