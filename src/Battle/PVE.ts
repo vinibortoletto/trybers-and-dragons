467d861038d047fbd06d941cfb18a5b323145ef3
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-17 14:59:02 -0300

    feat: pve

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,25 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  constructor(
+    private _player: Fighter,
+    private _enemyList: (Fighter | SimpleFighter)[],
+  ) {
+    super(_player);
+  }
+
+  fight(): number {
+    while (
+      this._player.lifePoints > 0 
+      && this._enemyList.some((enemy) => enemy.lifePoints > 0)
+    ) {
+      this._enemyList.forEach((enemy) => {
+        this._player.attack(enemy);
+        enemy.attack(this._player);
+      });
+    }
+
+    return super.fight();
+  }
+}
