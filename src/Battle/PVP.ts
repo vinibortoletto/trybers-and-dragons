4cb0302ea0ac463a4cd6a295af42397b9b1f5679
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-02-17 14:05:59 -0300

    feat: pvp

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,23 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(
+    private _player1: Fighter,
+    private _player2: Fighter,
+  ) {
+    super(_player1);
+  }
+
+  fight(): number {
+    while (
+      this._player1.lifePoints > 0 
+      && this._player2.lifePoints > 0
+    ) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);  
+    }
+
+    return super.fight();
+  }
+}
